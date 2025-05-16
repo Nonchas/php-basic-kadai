0ccdfc14b7972a3e119896d4df3d3456f92899e2
Nonchas <168254736+Nonchas@users.noreply.github.com>
2025-05-16 18:24:38 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+  </head>
+
+  <body>
+    <p>
+      <?php
+      class Food{
+        private $name;
+        private $price;
+
+        public function show_price(){
+          echo $this->price . '<br>';
+        }
+
+        public function __construct(string $name, int $price){
+          $this->name = $name;
+          $this->price = $price;
+        }
+      }
+
+      class Animal{
+        private $name;
+        private $height;
+        private $weight;
+
+        public function show_height(){
+          echo $this->height . '<br>';
+        }
+
+        public function __construct(string $name, int $height, int $weight){
+          $this->name = $name;
+          $this->height = $height;
+          $this->weight = $weight;
+        }
+      }
+
+      $potato = new Food('potato', 250);
+      $dog = new Animal('dog', 60, 5000);
+
+      echo print_r($potato).'<br>';
+      echo print_r($dog).'<br>';
+
+      $potato->show_price();
+      $dog->show_height();
+
+      ?> 
+    </p>
+  </body>
+</html>
